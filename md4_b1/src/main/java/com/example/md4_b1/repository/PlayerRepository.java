afacd04a1b1fc91a3cd83f7b6ad001ca326d1ea9
linh77564 <you@example.com>
2024-12-26 11:47:30 +0700

    up bai ss8

--- md4_b1/src/main/java/com/example/md4_b1/repository/PlayerRepository.java
+++ md4_b1/src/main/java/com/example/md4_b1/repository/PlayerRepository.java
@@ -13,11 +13,11 @@ public class PlayerRepository {
 
     private static List<Player> players = new ArrayList<>();
 
-    static {
-        players.add(new Player(1, "P001", "Trần Văn A", LocalDate.of(1995, 5, 20), "5 năm", "Tiền đạo", "https://example.com/anh1.jpg"));
-        players.add(new Player(2, "P002", "Nguyễn Văn B", LocalDate.of(1998, 8, 15), "3 năm", "Hậu vệ", "https://example.com/anh2.jpg"));
-        players.add(new Player(3, "P003", "Lê Thị C", LocalDate.of(2000, 12, 30), "2 năm", "Thủ môn", "https://example.com/anh3.jpg"));
-    }
+//    static {
+//        players.add(new Player(1, "P001", "Trần Văn A", LocalDate.of(1995, 5, 20), "5 năm", "Tiền đạo", "https://example.com/anh1.jpg"));
+//        players.add(new Player(2, "P002", "Nguyễn Văn B", LocalDate.of(1998, 8, 15), "3 năm", "Hậu vệ", "https://example.com/anh2.jpg"));
+//        players.add(new Player(3, "P003", "Lê Thị C", LocalDate.of(2000, 12, 30), "2 năm", "Thủ môn", "https://example.com/anh3.jpg"));
+//    }
 
     public List<Player> findAll() {
         List<Player> players = BaseRepository.entityManager.createQuery("from player", Player.class).getResultList();
